8df58648ba54a08f49232dddc5156cd6814c21aa
ScureHu <33121909+ScureHu@users.noreply.github.com>
2019-04-24 18:59:10 +0800

    Update SignDefaultController.java

--- sign/src/main/java/com/zcmu/sign/controller/SignDefaultController.java
+++ sign/src/main/java/com/zcmu/sign/controller/SignDefaultController.java
@@ -6,4 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sign")
 public class SignDefaultController {
+     @Autowired
+    private SignDefaultService signDefaultService;
+   /**
+     * 获取当前的患者当天的记录
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}" ,method = RequestMethod.GET )
+    public Result getTodaySign(@PathVariable String id){
+        List<SignDefault> singList = signDefaultService.getDateSign(id);
+        List<Date> dateList = new ArrayList<>();
+        for (SignDefault s :
+                singList) {
+            dateList.add(s.getMeasureTime());
+        }
+        return Result.success(dateList);
+    }
 }

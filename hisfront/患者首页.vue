ea171edea7a5ac9cbe00b8604bc6b74021243ae1
ScureHu <33121909+ScureHu@users.noreply.github.com>
2019-04-28 20:01:13 +0800

    Update 患者首页.vue

--- hisfront/患者首页.vue
+++ hisfront/患者首页.vue
@@ -1,164 +1,137 @@
 <template>
 <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-
-      <el-form-item label="姓名">
-        <el-input v-model="form.name" :disabled="true"></el-input>
-      </el-form-item>
-
-       <el-form-item label="床位号">
-          <el-select v-model="form.bed" placeholder="请选择" :disabled="true">
-            <el-option
-              v-for="item in beds"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-      </el-form-item>
-
-      <el-form-item label="性别">
-        <el-select v-model="form.sex" placeholder="选择性别" :disabled="true">
-          <el-option label="男" value="1"/>
-          <el-option label="女" value="2"/>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="年龄" >
-        <el-input v-model="form.age" :disabled="true"/>
-      </el-form-item>
-
-       <el-form-item label="诊断">
-        <el-input v-model="form.diagnosis" :disabled="true"/>
-      </el-form-item>
-
-      <el-form-item label="主治医生">
-        <el-input v-model="form.attnDoctor" :disabled="true"/>
-      </el-form-item>
-
-      <el-form-item label="身份证号">
-        <el-input v-model="form.idNo" :disabled="true"/>
-      </el-form-item>
-      
-      <el-form-item label="家庭住址">
-        <el-input v-model="form.homeAddress"/>
-      </el-form-item>
-
-      <el-form-item label="生日">
-        <el-date-picker
-            v-model="form.birthday"
-            type="date"
-            placeholder="选择日期"
-            :disabled="true"
-            >
-        </el-date-picker>
-      </el-form-item>
-
-      <el-form-item label="入院时间">
-        <el-date-picker
-            v-model="form.inAdmitTime"
-            type="date"
-            placeholder="选择日期"
-            :disabled="true"
-            >
-        </el-date-picker>
-      </el-form-item>
-
-     <el-form-item label="出院时间">
-        <el-date-picker
-            v-model="form.inAdmitTime"
-            type="date"
-            placeholder="选择日期">
-        </el-date-picker>
-      </el-form-item>
-
-      <el-form-item label="护理级别">
-        <el-select v-model="form.nursingLevel" placeholder="选择护理级别" :disabled="true">
-          <el-option label="一级护理" value="1"/>
-          <el-option label="二级护理" value="2"/>
-          <el-option label="三级护理" value="3"/>
-          <el-option label="特级护理" value="4"/>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="联系人">
-        <el-input v-model="form.contactName"/>
-      </el-form-item>
-
-      <el-form-item label="联系人电话">
-        <el-input v-model="form.contactPhone"/>
-      </el-form-item>
-
-      <el-form-item label="出院后由谁照顾">
-        <el-radio-group v-model="form.care">
-          <el-radio border label="配偶"></el-radio>
-          <el-radio border label="父母"></el-radio>
-          <el-radio border label="子女"></el-radio>
-          <el-radio border label="保姆"></el-radio>
-          <el-radio border label="其他"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-
-      <el-form-item label="出院后去向">
-        <el-radio-group v-model="form.forAddress">
-          <el-radio border label="家"></el-radio>
-          <el-radio border label="养老院"></el-radio>
-          <el-radio border label="社区"></el-radio>
-          <el-radio border label="敬老院"></el-radio>
-          <el-radio border label="其他"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">创建出院评估单</el-button>
-      </el-form-item>
+  <br>
+  <el-form :inline="true" class="demo-form-inline" label-width="80px">   
+        <el-form-item label="护理等级">
+             <el-select v-model="select" placeholder="请选择">
+                <el-option label="一级护理" value="1"></el-option>
+                <el-option label="二级护理" value="2"></el-option>
+                <el-option label="三级护理" value="3"></el-option>
+                <el-option label="特级护理" value="4"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-button type="primary" @click="fetchData()">查询</el-button>
     </el-form>
-</div>
+    <el-col :span="4" v-for="o in 18" :key="o">
+      <el-card class="box-card" shadow="hover">
+        <div slot="header" class="clearfix">
+            <span>病人姓名</span>
+            <el-button style="float: right; padding: 3px 0" type="text" @click="dialogFormVisible = true">录入体温</el-button>
+        </div>
+          <div v-for="o in 2" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+          </div>
+          <el-popover
+              placement="top-start"
+              title="详细信息"
+              width="180"
+              trigger="hover"
+              >
+                    {{'床号： ' + o }}
+                    <br>
+                     {{'姓名： ' + "mmomomo" }}
+                    <br>
+                    {{'年龄： ' + o }} 
+                    <br>
+                    {{'性别： ' + o }}
+                    <br>
+                    {{'身份证号： ' + o }}
+                    <br>
+                    {{'联系电话： ' + o }}
+                    <br>
+                    {{'住院时间： ' + o }}
+                    <br>
+                    {{'诊断： ' + o }}
+                    <br>
+                    <br>
+                    {{'临床科室： ' + o }}
+                    <br>
+                    {{'主治医生： ' + o }}
+                    <br>
+                    {{'护理级别： ' + o }}
+              <el-button slot="reference">详细信息</el-button>
+          </el-popover>
+        </el-card>
+    </el-col>
+    <div align="center" class="paginat"> 
+      <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="1000">
+      </el-pagination>
+    </div>
+    <el-dialog  title="生命体征录入" :visible.sync="dialogFormVisible" >
+      <el-form label-width="100px">
+
+        <el-form-item label="时间点" >  
+          <el-select v-model="from.date" placeholder="请选择测量时间点">
+            <el-option label="5:00" value="1"></el-option>
+            <el-option label="10:00" value="2"></el-option>
+            <el-option label="15:00" value="3"></el-option>
+            <el-option label="22:00" value="4"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="体温(℃)">
+            <el-input placeholder="体温" v-model="from.t">
+              <template slot="append">℃</template>
+            </el-input>
+        </el-form-item>
+        <el-form-item label="心率(分钟/次)">
+            <el-input  placeholder="心率(分钟/次)" v-model="from.t"></el-input>
+        </el-form-item>
+        <el-form-item label="脉搏(次/分)">
+            <el-input  placeholder="脉搏(次/分)" v-model="from.p"></el-input>
+        </el-form-item>
+        <el-form-item label="血压(mmHg)">
+            <el-input placeholder="格式为mm/Hg" v-model="from.bp"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" >保存</el-button>
+          <el-button @click="dialogFormVisible = false" >关闭</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        sex:'1',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: '',
-        bed:'1',
-        nursingLevel:'3'
-      },
-      beds:[
-        {
-          value: '1',
-          label: '1'
-        },
-        {
-          value: '2',
-          label: '2'
-        }
-      ]
-    }
-  },
-  methods: {
-    onSubmit() {
-      this.$message('submit!')
-    },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
+      select: '',
+      animalHeat:'',
+      dialogFormVisible: false,
+      from:{}
     }
   }
 }
 </script>
 
-<style scoped>
-.line{
-  text-align: center;
-}
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 180px;
+    margin-bottom: 18px;
+    
+  }
+  .paginat{
+    float: right ;
+    margin-bottom: 35px
+  }
 </style>
